7c0359114f84cfcc6e28ac11270f13313ab3e809
ZaneDev <alvferbarrero@gmail.com>
2025-05-10 18:19:10 +0200

    UIPath class added

---
+++ Assets/_Scripts/Libraries/Graphics/UIPath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Physics.Graphics
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    internal class UIPath : Graphic
+    {
+        [SerializeField] private List<Vector2> positions = new List<Vector2>();
+        [SerializeField] private float width = 5f;
+
+        internal void SetPoints(Vector2[] keyPositions)
+        {
+            positions.Clear();
+            positions.AddRange(keyPositions);
+
+            SetVerticesDirty();
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vertexHelper)
+        {
+            vertexHelper.Clear();
+
+            if (positions.Count < 2) return;
+
+            for (int i = 0; i < positions.Count - 1; i++)
+            {
+                Vector2 currentPoint = positions[i];
+                Vector2 nextPoint = positions[i + 1];
+
+                Vector2 direction = (nextPoint - currentPoint).normalized;
+                Vector2 normal = new Vector2(-direction.y, direction.x) * width * 0.5f;
+
+                Vector2 vertex0 = currentPoint + normal;
+                Vector2 vertex1 = currentPoint - normal;
+                Vector2 vertex2 = nextPoint - normal;
+                Vector2 vertex3 = nextPoint + normal;
+
+                int index = vertexHelper.currentVertCount;
+
+                vertexHelper.AddVert(vertex0, color, Vector2.zero);
+                vertexHelper.AddVert(vertex1, color, Vector2.zero);
+                vertexHelper.AddVert(vertex2, color, Vector2.zero);
+                vertexHelper.AddVert(vertex3, color, Vector2.zero);
+
+                vertexHelper.AddTriangle(index, index + 1, index + 2);
+                vertexHelper.AddTriangle(index + 2, index + 3, index);
+            }
+        }
+    }
+}
--- Assets/_Scripts/Tests.cs
+++ Assets/_Scripts/Tests.cs
@@ -3,6 +3,9 @@ using Physics.Planes;
 using Physics.Graphics;
 using System.Threading.Tasks;
 using Physics.Math;
+using Physics.Arrow;
+using Unity.VisualScripting;
+using System.Collections.Generic;
 
 [DisallowMultipleComponent] internal sealed class Tests : MonoBehaviour
 {
@@ -19,7 +22,33 @@ using Physics.Math;
             callback: CartesianPlane2D.ShowUpAnimated,
             color: new Color(1, 1, 1, 0)
         );
+
+        await Task.Delay(1000);
+
+        GameObject graph = new GameObject("Graph");
+        graph.transform.SetParent(GameObject.Find("Canvas").transform, false);
+
+        RectTransform graphTransform = graph.AddComponent<RectTransform>();
+        graphTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        graphTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        graphTransform.pivot = new Vector2(0.5f, 0.5f);
+        graphTransform.anchoredPosition = plane.GetPositionInPlane(0, 0);
+
+        UIPath path = graphTransform.AddComponent<UIPath>();
+
+        List<Vector2> keyPositions = new List<Vector2>();
+
+        for (float x = -3; x <= 3; x += 0.05f)
+        {
+            Vector2 position = plane.GetPositionInPlane(x, Function(x));
+            keyPositions.Add(new Vector2(-position.y, position.x));
+        }
+
+        path.SetPoints(keyPositions.ToArray());
     }
 
-    
+    private float Function(float x)
+    {
+        return Mathf.Sin(x);
+    }
 }
